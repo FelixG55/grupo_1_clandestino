7916cfdb603d122cbd85f7c97194fe4e2b0f0283
DobleA74 <andresandriani@gmail.com>
2023-01-10 21:39:06 -0300

    tercer intento push

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Respondiendo en el puerto 30000'));
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'))
+})
+
+app.get('/productCart2', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/productCart2.html'))
+})
+
+app.listen(3000, () => console.log('Respondiendo en el puerto 3000'));
 app.get('/', (req,res) => {
 
     res.sendFile(path.join(__dirname, './views/index.html'))
