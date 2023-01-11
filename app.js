067d907ca75b9268eef39bb045872c95df95de49
DobleA74 <andresandriani@gmail.com>
2023-01-10 23:11:52 -0300

    agregue productos

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
-
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 })
 
+app.get('/productDetail', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
 
 
 app.listen(3030, ()=>{
     console.log('Servidor corriendo en el puerto 3030');
 })
+
+app.use(express.static('public'));
+
+
