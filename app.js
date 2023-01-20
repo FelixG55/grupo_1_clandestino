57913eeb11b1ad5268388b3b411a72460e9d52f3
grondoneitor <facundo.grondona.02@gmail.com>
2023-01-19 22:16:50 -0300

    agregando

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3030, () => console.log('Respondiendo en el puerto 3030'));
+app.listen(3000, () => console.log('Respondiendo en el puerto 3030'));
 app.get('/', (req,res) => {
 
     res.sendFile(path.join(__dirname, './views/home.html'))
