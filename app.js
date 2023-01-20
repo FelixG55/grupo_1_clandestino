5a7f378928db0878f09a70daa893d8e0d892d6fe
feluta555 <fei555@msn.com>
2023-01-19 21:57:19 -0500

    cambio en index

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
-
-
+        
 app.get('/productCart2', (req,res) => {
     res.sendFile(path.join(__dirname, './views/productCart2.html'))
 })
