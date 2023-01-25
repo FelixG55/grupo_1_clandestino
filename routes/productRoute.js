aff6beafe4d7b698c8120a3d45345d8008c575ab
feluta555 <fei555@msn.com>
2023-01-24 19:40:30 -0500

    rep actualizado con carpetas

--- routes/productRoute.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-
-const {productCart, productDetail} = require ('../controllers/productController');
-const routerProduct = express.Router();
-
-routerProduct.get ('/productCart', productCart);
-routerProduct.get ('/productDetail', productDetail);
-
-module.exports = routerProduct;
