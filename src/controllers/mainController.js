e0cbd1fd8b4f3167505942aafeb261e9ced17bb2
SebGom1983 <119420661+SebGom1983@users.noreply.github.com>
2023-02-02 21:48:52 -0500

    trabajamos en css y agregamos video a index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 const index = (req, res) => {
-    res.render(path.join(__dirname, '../views/index'),({style: "styles-index" }));
+    res.render(path.join(__dirname, '../views/index'),({style: "styles-index"}));
 };
 
 const location = (req, res) => {
     res.render(path.join(__dirname, '../views/location'),{style: "styles-location"});
 };
+
+
 
 module.exports = {
     index,
