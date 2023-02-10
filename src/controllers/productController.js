8a39804cae176e1999b4eda2ee0e4a04a6c95182
feluta555 <fei555@msn.com>
2023-02-09 21:25:44 -0500

    Cambios a las rutas, view el formulario de creación,view del detalle del producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,22 +1,40 @@
-
+const fs = require('fs');
 const path = require('path');
+const productsFilePath = path.join(__dirname, '../database/productos.json');
+const allProducts = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
 
 const productCart= (req, res) => {
+
+    req.
     res.render(path.join(__dirname, '../views/productCart'),{style: "styles-productCart"});
 };
 
-
-
 const productEdit = (req, res) => {
+
     res.render(path.join(__dirname, '../views/productEdit'),{style: "styles-productEdit"});
 };
-const products = (req, res) =>{};
+const products = (req, res) =>{
+
+    res.render(path.join(__dirname, '../views/productDetail'),{style: "styles-productDetail"})
+};
 
 const postProducts = (req, res) =>{};
 
-const createProducts = (req, res) =>{};
+const createProducts = (req, res) =>{
 
-const getIdProducts = (req, res) =>{};
+    res.render(path.join(__dirname, '../views/createProduct'),{style: "styles-createProduct"});
+};
+
+const getProduct = (req, res) =>{
+    const {id} = req.params;
+    const product = allProducts.find(elem => elem.id == id);
+    res.render(path.join(__dirname, '../views/productIdDetail'),{product ,style: "styles-productIdDetail"});
+
+};
+
 
 const putProducts = (req, res) =>{};
 
@@ -24,7 +42,13 @@ const productDetail = (req, res) =>{
     res.render(path.join(__dirname, '../views/productDetail'),{style: "styles-productDetail"});
 };
 
- 
+const editProduct = (req, res) => {
+
+};
+const delivery = (req, res) => {
+    res.render  (path.join(__dirname, '../views/productCart'),{style: "styles-productCart"})
+};
+
 module.exports = {
     // productCart,
     // productEdit,
@@ -32,7 +56,10 @@ module.exports = {
     products, 
     postProducts,
     createProducts,
-    getIdProducts,
+    getProduct,
     putProducts,
+    editProduct,
+    delivery,
+
    
 };
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,14 +1,17 @@
 const express= require('express');
 
-const {products, postProducts,createProducts,getIdProducts,putProducts} = require ('../controllers/productController');
+const {products, postProducts,createProducts,getIdProducts,putProducts, editProduct, delivery, getProduct} = require ('../controllers/productController');
 const routerProduct = express.Router();
 
 routerProduct.get('/products', products);
 routerProduct.post('/products', postProducts);
 routerProduct.get('/products/create', createProducts);
-routerProduct.get('/products/:id', getIdProducts);
+routerProduct.get('/products/:id', getProduct);
+routerProduct.get('/products/:id/edit',editProduct);
 routerProduct.put('/products/:id', putProducts);
 routerProduct.delete('/products/:id', putProducts);
+
+routerProduct.get('/productCart', delivery)
 
 // routerProduct.get('/productCart', productCart);
 // routerProduct.get('/productDetail', productDetail);
