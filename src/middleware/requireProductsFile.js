e9665c9ab3c6b46241db71f72025cc74933ec0c4
feluta555 <fei555@msn.com>
2023-02-22 20:47:56 -0500

    Muchos cambios al repo

---
+++ src/middleware/requireProductsFile.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// const readProductsFile = (req, res, next){
+
+//     const productsFilePath = path.join(__dirname, '../database/productos.json');
+//     const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+//     res.send = 
+// }
+
+// module.exports = products;
