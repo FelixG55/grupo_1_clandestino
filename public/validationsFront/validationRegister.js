fb5defe5fb14a9b777315eacd36b89c348208200
DobleA74 <andresandriani@gmail.com>
2023-04-25 10:13:27 -0300

    prueba de validaciones

--- public/validationsFront/validationRegister.js
+++ public/validationsFront/validationRegister.js
@@ -1,24 +1,36 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let formRegister = document.getElementById('formRegister');
-    let errores =[]
-    formRegister.addEventListener('submit',function (event){
+    let errores = []
+    formRegister.addEventListener('submit', function (event) {
+
         
-        let campoNombre = document.getElementById('name');
+        let campoNombre = document.getElementById('name', 'lastname');
         console.log(campoNombre);
         if (campoNombre.value == "") {
             console.log(campoNombre.value);
             errores.push("Campo obligatorio");
-        }else if(campoNombre.value.length >= 2){
+        } else if (campoNombre.value.length > 2) {
             errores.push("Debe tener al menos dos caracteres")
-        }    
-        if(errores.length > 0){
+        };
+
+        let campoEmail = document.getElementById('email')
+        
+        if (campoEmail.value == "") {
+            console.log(campoEmail.value);
+            errores.push("Campo obligatorio");
+        } else if (campoEmail.value.length > 2) {
+            errores.push("Debe tener al menos dos caracteres")
+        }
+
+        if (errores.length > 0) {
             event.preventDefault();
             let ulErrores = document.querySelector(".error-msg ul");
-            errores.forEach(error =>{
+            errores.forEach(error => {
                 ulErrores.innerHTML += `<li> ${error} </li>`
             });
-        }    
+        }
     })
 
-   console.log();
+
+    console.log();
 })
