7579be32b4cc93ccb926da77ea9734f413f76769
DobleA74 <andresandriani@gmail.com>
2023-04-25 22:39:59 -0300

    Formulario de validaciones Register

--- public/validationsFront/validationRegister.js
+++ public/validationsFront/validationRegister.js
@@ -1,36 +1,86 @@
 window.addEventListener('load', function () {
     let formRegister = document.getElementById('formRegister');
-    let errores = []
     formRegister.addEventListener('submit', function (event) {
 
-        
-        let campoNombre = document.getElementById('name', 'lastname');
-        console.log(campoNombre);
+        //Campo Name
+        let erroresName = []
+        let campoNombre = document.getElementById('name');
+
         if (campoNombre.value == "") {
-            console.log(campoNombre.value);
-            errores.push("Campo obligatorio");
-        } else if (campoNombre.value.length > 2) {
-            errores.push("Debe tener al menos dos caracteres")
+            erroresName.push("El campo nombre es obligatorio");
+        } if (campoNombre.value.length < 2) {
+            erroresName.push("El campo nombre debe tener al menos dos caracteres")
         };
 
-        let campoEmail = document.getElementById('email')
-        
-        if (campoEmail.value == "") {
-            console.log(campoEmail.value);
-            errores.push("Campo obligatorio");
-        } else if (campoEmail.value.length > 2) {
-            errores.push("Debe tener al menos dos caracteres")
-        }
-
-        if (errores.length > 0) {
+        if (erroresName.length > 0) {
             event.preventDefault();
-            let ulErrores = document.querySelector(".error-msg ul");
-            errores.forEach(error => {
+            let ulErrores = document.querySelector(".error-msg-name ul");
+            console.log(erroresName);
+            erroresName.forEach(error => {
                 ulErrores.innerHTML += `<li> ${error} </li>`
             });
         }
-    })
+        //Campo lastname
+        let erroresLastname = []
+        let campoLastname = document.getElementById('lastname');
+
+        if (campoLastname.value == "") {
+            erroresLastname.push("El campo apellido es obligatorio");
+        }if (campoLastname.value.length < 2) {
+            erroresLastname.push("El campo apellido debe tener al menos dos caracteres")
+        };
+        if (erroresLastname.length > 0) {
+            event.preventDefault();
+            let ulErrores = document.querySelector(".error-msg-lastname ul");
+            erroresLastname.forEach(error => {
+                ulErrores.innerHTML += `<li> ${error} </li>`
+            });
+        }
+
+        //Campo Email
+        let erroresEmail = [];
+        let campoEmail = document.getElementById('email');
+        var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+        if (campoEmail.value == "") {
+            erroresEmail.push("El campo Email es obligatorio");
+        } if (!validEmail.test(campoEmail.value)) {
+            erroresEmail.push("El campo Email debe ser valido");
+
+        if (erroresEmail.length > 0) {
+           event.preventDefault();
+           let ulErrores = document.querySelector(".error-msg-Email ul");
+           erroresEmail.forEach(error => {
+           ulErrores.innerHTML += `<li> ${error} </li>`
+           
+                });
+            }
+        }
+        //Campo contraseña
+        let erroresPassword = []
+        let campoPassword = document.getElementById('password');
+
+        if (campoPassword.value == "") {
+            erroresPassword.push("El campo password es obligatorio");
+        } if (campoPassword.value.length < 8) {
+            erroresPassword.push("El campo password debe tener al menos ocho caracteres")
+        };
+        if (erroresPassword.length > 0) {
+            event.preventDefault();
+            let ulErrores = document.querySelector(".error-msg-password ul");
+            erroresPassword.forEach(error => {
+                ulErrores.innerHTML += `<li> ${error} </li>`
+            });
+        }
+        //Campo imagen(falta esto)
+        let erroresImagen = []
+        let campoImagen = document.getElementById('image');
+
+        if (campoImagen.value) {
+            erroresImagen.push("La imagen debe ser en formato JPG, JPEG, PNG, GIF");
+        } 
+      
+    
+})})
 
 
-    console.log();
-})
