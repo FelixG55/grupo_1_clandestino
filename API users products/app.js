59ad9624558593787917b26b03f89466df5b3b3a
grondoneitor <facundo.grondona.02@gmail.com>
2023-04-28 23:08:32 -0300

    agregando API de users y productos

---
+++ API users products/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app  = express()
+const path = require("path")
+
+const port = process.env.PORT || 3022
+app.listen(port, (req, res) => console.log(`Se esta corriendo el servidor ${port}`) )
+app.set("view engine", "ejs")
+app.use(express.json())
+
+const usersRoutes = require("./src/routes/userRoutes.js") 
+const productsRoutes = require("./src/routes/productsRoutes.js") 
+
+app.use("/api",usersRoutes)
+app.use("/api", productsRoutes)
---
+++ API users products/src/routes/productsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const Route = express.Router()
+const {products, product, categori} = require("../controllers/productsControllers")
+
+Route.get("/products", products)
+Route.get("/products/:id", product)
+Route.get("/categori/:id", categori)
+module.exports = Route
