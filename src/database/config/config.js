4170174bac416ea2d339fada5b71383716e67732
SebGom1983 <119420661+SebGom1983@users.noreply.github.com>
2023-05-10 21:23:13 -0500

    añadimos add product y css

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "",
+    "password": "CSC03gdcc09",
     "database": "clandestino",
     "host": "127.0.0.1",
     "dialect": "mysql"
